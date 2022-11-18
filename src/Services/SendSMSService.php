d96bfd1b284f1dc13a5abb8bfc8855382f846a96
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-11-18 09:21:39 +0330

    updated SendSMSService
    now we pass custom template to sendOTP and multiple params

--- src/Services/SendSMSService.php
+++ src/Services/SendSMSService.php
@@ -2,13 +2,14 @@
 
 namespace Genocide\Radiocrud\Services;
 
-use Genocide\Radiocrud\Services\SendHTTPRequestService;
-
 class SendSMSService
 {
-    private $line_numbers = [];
-
-    public function send ($receptor_numbers, string $message)
+    /**
+     * @param $receptor_numbers
+     * @param string $message
+     * @return SendHTTPRequestService
+     */
+    public function send ($receptor_numbers, string $message): SendHTTPRequestService
     {
         if (is_array($receptor_numbers))
         {
@@ -28,23 +29,35 @@ class SendSMSService
             ->send();
     }
 
-    public function send_otp ($receptor_numbers, $param1)
+    /**
+     * @param string|array $receptorNumbers
+     * @param string $template
+     * @param string ...$params
+     * @return SendHTTPRequestService
+     */
+    public function sendOTP (string|array $receptorNumbers, string $template, string ...$params): SendHTTPRequestService
     {
-        if (is_array($receptor_numbers))
+        if (is_array($receptorNumbers))
         {
-            $receptor_numbers = implode(",", $receptor_numbers);
+            $receptorNumbers = implode(",", $receptorNumbers);
+        }
+
+        $requestBody = [
+            'receptor' => $receptorNumbers,
+            'type' => '1',
+            'template' => $template,
+        ];
+
+        foreach ($params AS $index => $param)
+        {
+            $requestBody['param' . ($index+1)] = $param;
         }
 
         return (new SendHTTPRequestService())->set_url("https://api.ghasedak.me/v2/verification/send/simple")
             ->set_headers([
                 "apikey:" . $this->get_api_key()
             ])
-            ->set_body([
-                'receptor' => $receptor_numbers,
-                'type' => '1',
-                'template' => 'ForgotPassword',
-                'param1' => $param1
-            ])
+            ->set_body($requestBody)
             ->send();
     }
 
