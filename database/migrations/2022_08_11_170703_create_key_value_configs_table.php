8c432668c08ebdb7b1af3db6df4f641429127b5e
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-12-04 11:28:00 +0330

    added key_value configs migration

---
+++ database/migrations/2022_08_11_170703_create_key_value_configs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('key_value_configs', function (Blueprint $table) {
+            $table->id();
+            $table->string('key', 255);
+            $table->text('value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('key_value_configs');
+    }
+};
