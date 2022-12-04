7b4f8615fd6de5d5ec2e551d02afb98b6ddbbf5c
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-12-04 12:41:29 +0330

    added service provider

---
+++ src/RadiocrudServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Genocide\Radiocrud;
+
+use Illuminate\Support\ServiceProvider;
+
+class RadiocrudServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../database/migrations/2022_08_11_170703_create_key_value_configs_table.php' => database_path('migrations')
+        ], 'migrations');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
